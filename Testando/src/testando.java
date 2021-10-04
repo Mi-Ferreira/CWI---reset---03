59c5b4df6664816c17c1ee3d56bb67d16eebbb94
Michele <michelecarolineferreiira@hotmail.com>
2021-10-04 14:13:40 -0300

    Adicionado Desafio

---
+++ Testando/src/testando.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class testando {
+
+    public static void main(String[] args){
+    System.out.print("Oi");
+        }
+    }
