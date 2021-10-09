d2df0ce3b9dc9f82113d97c0913d5a2979378159
Michele <michelecarolineferreiira@hotmail.com>
2021-10-09 15:05:53 -0300

    Exercicio 3

--- Aula_02/src/main/java/Exercicio_Aula02/Aplicacao.java
+++ Aula_02/src/main/java/Exercicio_Aula02/Aplicacao.java
@@ -2,13 +2,18 @@ package Exercicio_Aula02;
 
 public class Aplicacao {
     public static void main(String[] args) {
-        Diretor diretor = new Diretor ("Jon Watts", 40,7,Genero.MASCULINO);
+        /*Diretor diretor = new Diretor ("Jon Watts", 40,7,Genero.MASCULINO);
         diretor.imprimirInformacoes();
         Filme f1 = new Filme ("Homem Aranha de volta ao Lar","SuperHeroi", 133,2017,5.0,diretor);
         f1.reproduzir();
 
         Filme f2 = new Filme ("Homem Aranha Longe de Casa","SuperHeroi", 250,2029,4.2,diretor);
-        f2.reproduzir();
+        f2.reproduzir();*/
 
+        Pessoa d1 = new Diretor("Jon Watts", 40,Genero.MASCULINO);
+        d1.imprimirInformacoes();
+
+        Pessoa a1 = new Ator("Tom Holland", 25, Genero.MASCULINO);
+        a1.imprimirInformacoes();
     }
 }
